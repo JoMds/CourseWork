24ee0b85e176932902ccf70a06ff35d52237f20c
JoMds <Giannis58@Hotmail.com>
2022-11-13 19:21:26 +0200

    upd

--- src/main/java/com/mycompany/coursework/Person.java
+++ src/main/java/com/mycompany/coursework/Person.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.coursework;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author giann
@@ -12,26 +14,63 @@ public class Person {
     
     private String name;
     private String gender;
+    private String fathersName;
+    private String mothersName;
+    private ArrayList<String> children;
     
-    public Person(String name, String gender){
-    this.name = name;
-    this.gender = gender;
-}
-    
-    public String getName(){
+    public Person(String name, String gender, String fathersName, String mothersName) {
+        this.name = name;
+        this.gender = gender;
+        this.fathersName = fathersName;
+        this.mothersName = mothersName;
+        children = new ArrayList<String>();
+    }
+
+    public String getName() {
         return name;
     }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
-    public String getGender(){
+
+    public String getGender() {
         return gender;
     }
-    
+
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public String getMothersName() {
+        return mothersName;
+    }
+
+    public ArrayList<String> getChildren() {
+        return children;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setGender(String gender) {
-        this.gender= gender;
-    }   
+        this.gender = gender;
+    }
+
+    public void setFathersName(String fathersName) {
+        this.fathersName = fathersName;
+    }
+
+    public void setMothersName(String mothersName) {
+        this.mothersName = mothersName;
+    }
+
+    public void setChildren(ArrayList<String> children) {
+        this.children = children;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", gender=" + gender + ", fathersName=" + fathersName + ", mothersName=" + mothersName + ", children=" + children + '}';
+    }
+    
+    
 }
     
