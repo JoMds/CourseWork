7e4f53ce61a9b71ff66d17673ba2eb4b7de4acd4
JoMds <Giannis58@Hotmail.com>
2022-11-13 18:59:12 +0200

    person class

---
+++ src/main/java/com/mycompany/coursework/Person.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.coursework;
+
+/**
+ *
+ * @author giann
+ */
+public class Person {
+    
+    private String name;
+    private String gender;
+    
+    public Person(String name, String gender){
+    this.name = name;
+    this.gender = gender;
+}
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getGender(){
+        return gender;
+    }
+    
+    public void setGender(String gender) {
+        this.gender= gender;
+    }   
+}
+    
