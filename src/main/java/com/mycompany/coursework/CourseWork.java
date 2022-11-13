728700c934876b93b2fc26aa8d82863475abb905
JoMds <Giannis58@Hotmail.com>
2022-11-13 19:28:16 +0200

    upd

--- src/main/java/com/mycompany/coursework/CourseWork.java
+++ src/main/java/com/mycompany/coursework/CourseWork.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,12 +20,18 @@ public class CourseWork {
     public static void main(String[] args) {
            //change to your specific file path 
             String file ="C:\\Users\\giann\\Documents\\NetBeansProjects\\CourseWork\\src\\main\\java\\file\\tree.csv";
+            // var for csv line            
             String line ="";
+           //split indicator 
+            String split="";
+           //ArrayList with persons             
+            ArrayList<Person> persons = new ArrayList<Person>();
+            
         try{
              BufferedReader br = new BufferedReader(new FileReader(file));
              while((line = br.readLine())!=null){
                  System.out.println(line);
-                 String neo [] =line.split("");
+                 String neo [] =line.split(split);
 //                 for(String n : neo){
 //                 System.out.println(n);
 //                 }
