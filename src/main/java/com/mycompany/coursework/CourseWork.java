1f22dae71a1fba040b199e9e2ff9cfc92c7c14d7
JoMds <Giannis58@Hotmail.com>
2022-11-20 00:55:56 +0200

    array display

--- src/main/java/com/mycompany/coursework/CourseWork.java
+++ src/main/java/com/mycompany/coursework/CourseWork.java
@@ -19,11 +19,13 @@ public class CourseWork {
 
     public static void main(String[] args) {
            //change to your specific file path 
-            String file ="C:\\Users\\giann\\Documents\\NetBeansProjects\\CourseWork\\src\\main\\java\\file\\tree.csv";
+            String file ="C:\\Users\\giann\\Documents\\NetBeansProjects\\CourseWork\\src\\main\\java\\file\\tree_V2.csv";
             // var for csv line            
             String line ="";
            //split indicator 
-            String split="\\s+";
+//            String split="\\s+";
+            String split =",";
+//            String man ="man";
            //ArrayList with persons             
             ArrayList<Person> persons = new ArrayList<Person>();
             
@@ -33,20 +35,44 @@ public class CourseWork {
 //                 System.out.println(line); 
                  
                  String words [] = line.split(split);
+                     
+                     if(line.contains("spouse") || line.contains("man")){
+                         for (int i=0; i<words.length +1; i++) {
+                             if (words.length!=2){
+                             System.out.println("Peron:"+"\t"+words[0]+"0"+words[1]+"\t" +"1"+"\t"+words[2]+"\t"+"2");
+//                             System.out.print(words[i]);
+                             }
+                          
+                         }
+                     }
+                     
+                    if(line.contains("mother")||line.contains("father") ){
+                         for (int i=0; i<words.length; i++) {
+                          System.out.println("Mother-Father:"+"\t"+words[0]+"0"+words[1]+"\t" +"1"+"\t"+words[2]+"\t"+"2");
+//                            System.out.print(words[i]);
+                         }
+                     }
+                     
+//                     if(line.contains("man")){
+//                         for (int i=0; i<words.length; i++) {
+//                          System.out.println("\t" + words[0]+"0"+"\t"+words[1]+"1");
+//                         }
+//                     }
                  
-                 for(int i =0 ; i<words.length; i++){
-                 words[i] = words[i].replaceAll("[^\\w]", "");
-//                 System.out.println(words.length);
-                 try{
-                 System.out.println("The name is: "+words[0]+" , the surname is;  "
-                  +words[1]+", the role is: "+words[2]+","
-                + " has an affair with:"+words[3]+" children of: "+words[4]);
-                 
-                 }catch(ArrayIndexOutOfBoundsException e){
-                   
-                 System.out.println(e);
-                 }
-                }
+//                 for(int i =0 ; i<words.length; i ++){
+//                     if(words.length!=2){
+//                         if( words[i].contains("spouse")){
+//                             System.out.println(words[i]+"spouse");
+//                         }
+//                         if(words[i].contains("mother") || words[i].contains("father"))
+//                              System.out.println(words[i]+"fater/mother");
+//                         System.out.println(words[0]+"[0]"+words[1]+"[1]"+words[2]+"[2]");
+//                     }
+//                     if(words[i].contains("man")){
+//                         System.out.println(words[i]+"test");
+//                     }
+//                       System.out.println(words[0]+"[0]"+words[1]+"[1]");
+//                 }
              }
         }catch(IOException e){
             System.out.print("Problem with file" + e);
