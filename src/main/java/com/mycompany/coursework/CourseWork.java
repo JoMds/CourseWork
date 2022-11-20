62e87dbaf1c10823d71f4f0e5cf8f5324ddef3de
JoMds <Giannis58@Hotmail.com>
2022-11-20 15:05:10 +0200

    array display

--- src/main/java/com/mycompany/coursework/CourseWork.java
+++ src/main/java/com/mycompany/coursework/CourseWork.java
@@ -10,72 +10,81 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author giann
  */
 public class CourseWork {
+    
+//    Arraylist
 
     public static void main(String[] args) {
            //change to your specific file path 
             String file ="C:\\Users\\giann\\Documents\\NetBeansProjects\\CourseWork\\src\\main\\java\\file\\tree_V2.csv";
-            // var for csv line            
-            String line ="";
-           //split indicator 
-//            String split="\\s+";
+            String line = "";
             String split =",";
-//            String man ="man";
-           //ArrayList with persons             
+            ArrayList<String[]> data = new ArrayList<String[]>();
             ArrayList<Person> persons = new ArrayList<Person>();
             
         try{
              BufferedReader br = new BufferedReader(new FileReader(file));
+             
              while((line = br.readLine())!=null){
-//                 System.out.println(line); 
+                 String words[] = line.split(split);
+                 data.add(words);
+             }
+             
+             String[] uniqueNames = getUniqueNames(data);
+             
+             for(String uname: uniqueNames){
+                 ArrayList<String[]> rows = (ArrayList<String[]>)data.stream().filter(x -> x[0].equals(uname)).collect(Collectors.toList());
                  
-                 String words [] = line.split(split);
+                 Person p = new Person();
+                
+                 for(String[] r : rows){
+                     switch(r.length){
                      
-                     if(line.contains("spouse") || line.contains("man")){
-                         for (int i=0; i<words.length +1; i++) {
-                             if (words.length!=2){
-                             System.out.println("Peron:"+"\t"+words[0]+"0"+words[1]+"\t" +"1"+"\t"+words[2]+"\t"+"2");
-//                             System.out.print(words[i]);
+                         case 2:
+                             p.setName(r[0]);
+                             p.setGender(r[1]);
+                             break;
+                         case 3:
+                             if(r[1].equals("spouse")){
+                                  p.setSpouseName(r[2]);
+                                  p.setRelathion(r[1]);
                              }
-                          
-                         }
+                             else if(r[1].equals("mother")||r[1].equals("father")){
+                                 p.addChild(r[2]);
+                             }
+                            break;
                      }
-                     
-                    if(line.contains("mother")||line.contains("father") ){
-                         for (int i=0; i<words.length; i++) {
-                          System.out.println("Mother-Father:"+"\t"+words[0]+"0"+words[1]+"\t" +"1"+"\t"+words[2]+"\t"+"2");
-//                            System.out.print(words[i]);
-                         }
-                     }
-                     
-//                     if(line.contains("man")){
-//                         for (int i=0; i<words.length; i++) {
-//                          System.out.println("\t" + words[0]+"0"+"\t"+words[1]+"1");
-//                         }
-//                     }
-                 
-//                 for(int i =0 ; i<words.length; i ++){
-//                     if(words.length!=2){
-//                         if( words[i].contains("spouse")){
-//                             System.out.println(words[i]+"spouse");
-//                         }
-//                         if(words[i].contains("mother") || words[i].contains("father"))
-//                              System.out.println(words[i]+"fater/mother");
-//                         System.out.println(words[0]+"[0]"+words[1]+"[1]"+words[2]+"[2]");
-//                     }
-//                     if(words[i].contains("man")){
-//                         System.out.println(words[i]+"test");
-//                     }
-//                       System.out.println(words[0]+"[0]"+words[1]+"[1]");
-//                 }
+                 }
+                 persons.add(p);
              }
         }catch(IOException e){
             System.out.print("Problem with file" + e);
         }
+        var pp = persons;
+        var x="";
+    }
+    
+    public  static String[] getUniqueNames(ArrayList<String[]> data){
+        
+        ArrayList<String> names = new ArrayList<String>();
+ 
+        data.stream().forEach(x -> {
+            names.add(x[0]);
+        });
+        
+        HashSet<String> hashSet = new HashSet<String>(names);
+        
+        var uniqueNames = hashSet.toArray(String[]::new);
+        
+        return uniqueNames;
     }
 }
+
